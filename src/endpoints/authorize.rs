f140784806f173f3cdf44226f15f1372731fca1a
Hannes Herrmann <hannes@eigenbasis.de>
2024-06-08 00:59:56 +0200

    parse auth endpoint special parameters

--- src/endpoints/authorize.rs
+++ src/endpoints/authorize.rs
@@ -10,7 +10,7 @@ use std::str::FromStr;
 #[derive(Deserialize)]
 struct RawAuthorizeParameters {
     scope: String,
-    response_type: String,
+    response_type: ResponseType,
     client_id: String,
     #[serde(deserialize_with = "parse_uri")]
     redirect_uri: Uri,
@@ -21,11 +21,44 @@ struct RawAuthorizeParameters {
     code_challenge_method: CodeChallengeMethod,
 }
 
+struct ResponseType {
+    code: bool,
+    id_token: bool,
+    token: bool,
+}
+
+impl<'de> Deserialize<'de> for ResponseType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s: &str = Deserialize::deserialize(deserializer)?;
+        let mut response_type = ResponseType {
+            code: false,
+            id_token: false,
+            token: false,
+        };
+        for single_response_type in s.split(' ') {
+            match single_response_type {
+                "code" => response_type.code = true,
+                "id_token" => response_type.id_token = true,
+                "token" => response_type.token = true,
+                other => {
+                    return Err(D::Error::custom(format!(
+                        "Invalid repsonse type '{}'",
+                        other
+                    )))
+                }
+            }
+        }
+        Ok(response_type)
+    }
+}
+
 fn parse_uri<'de, D>(deserializer: D) -> Result<Uri, D::Error>
 where
     D: Deserializer<'de>,
 {
-    // custom parsing logic here
     let s: &str = Deserialize::deserialize(deserializer)?;
     Uri::from_str(s).map_err(|e| D::Error::custom(e.to_string()))
 }
