16ef9582b10b2a497ebda0338b6c89a7d8e0a9a1
Hannes Herrmann <hannes@eigenbasis.de>
2024-06-07 17:08:28 +0200

    implement well-known endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,37 @@
-use axum::Router;
+use crate::endpoints::wellknown_endpoint;
 use axum::routing::get;
+use axum::Router;
+use std::ops::Deref;
+use std::sync::Arc;
+use url::Url;
+
+mod endpoints;
 
 pub fn create_app() -> Router {
-    Router::new().route("/", get(root))
+    let state = Config(Arc::new(InnerConfig {
+        issuer: Url::parse("http://localhost:3000").unwrap(),
+    }));
+
+    Router::new()
+        .route("/", get(root))
+        .route("/.well-known/openid-configuration", get(wellknown_endpoint))
+        .with_state(state)
 }
 // basic handler that responds with a static string
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+#[derive(Clone)]
+struct Config(Arc<InnerConfig>);
+
+impl Deref for Config {
+    type Target = InnerConfig;
+    fn deref(&self) -> &Self::Target {
+        self.0.deref()
+    }
+}
+
+struct InnerConfig {
+    issuer: Url,
 }
